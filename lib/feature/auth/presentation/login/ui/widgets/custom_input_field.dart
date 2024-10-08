d97a360be31f8005d4c4f815b4bfe0480638d089
ali72-20 <as20201118@gmail.com>
2024-10-09 02:32:17 +0300

    + add custom input text field

---
+++ lib/feature/auth/presentation/login/ui/widgets/custom_input_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomInputField extends StatelessWidget {
+  final String hintText;
+  final String labelText;
+  const CustomInputField({super.key, required this.hintText,required this.labelText});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: labelText,
+        labelStyle: Theme.of(context).inputDecorationTheme.labelStyle,
+        enabledBorder: Theme.of(context).inputDecorationTheme.enabledBorder,
+        focusedBorder: Theme.of(context).inputDecorationTheme.focusedBorder,
+        errorBorder: Theme.of(context).inputDecorationTheme.errorBorder,
+        disabledBorder: Theme.of(context).inputDecorationTheme.disabledBorder,
+        errorStyle: Theme.of(context).inputDecorationTheme.errorStyle,
+      ),
+    );
+  }
+}
