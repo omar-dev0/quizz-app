137cc16ebad5524ea5cb990a3b586899f482d39c
ali72-20 <as20201118@gmail.com>
2024-10-09 01:19:35 +0300

    + add AuthRepository + add login method

---
+++ lib/feature/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,7 @@
+import 'package:quizz_app/feature/auth/domain/common/api_result.dart';
+import '../model/user.dart';
+
+abstract class AuthRepository{
+  Future<Result<User?>> login(String email,String password);
+
+}
