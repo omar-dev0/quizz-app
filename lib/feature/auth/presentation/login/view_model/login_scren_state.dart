4ab73fe3f7567b94c23a1b77e7685bfb72ed786f
ali72-20 <as20201118@gmail.com>
2024-10-09 03:14:27 +0300

    + add login screen states

---
+++ lib/feature/auth/presentation/login/view_model/login_scren_state.dart
@@ -0,0 +1,16 @@
+import '../../../domain/model/user.dart';
+
+sealed class LoginScreenState{}
+
+class InitialScreenState extends LoginScreenState{}
+
+class LoadingState extends LoginScreenState{}
+class RememberMeCheckedBoxState extends LoginScreenState{}
+class LoginErrorState extends LoginScreenState{
+  Exception? exception;
+  LoginErrorState(this.exception);
+}
+class LoginSuccessState extends LoginScreenState{
+  User? user;
+  LoginSuccessState(this.user);
+}
