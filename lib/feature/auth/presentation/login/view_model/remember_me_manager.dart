3c65b410cc0459283746739f7c07c98ad5757afe
ali72-20 <as20201118@gmail.com>
2024-10-09 16:58:58 +0300

    remove unimportant remember me manger

--- lib/feature/auth/presentation/login/view_model/remember_me_manager.dart
+++
@@ -1,9 +0,0 @@
-class RememberMeManager {
-  bool _isRememberMeBoxChecked = false;
-
-  void setRememberMeBoxState(bool isRememberMeBoxChecked) {
-    _isRememberMeBoxChecked = isRememberMeBoxChecked;
-  }
-
-  bool get isRememberMeBoxChecked => _isRememberMeBoxChecked;
-}
