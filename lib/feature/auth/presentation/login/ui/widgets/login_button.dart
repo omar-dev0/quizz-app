08f0ddd44b95c7f3f864187dfcf743bc80f3d0ca
ali72-20 <as20201118@gmail.com>
2024-10-09 12:29:44 +0300

    add login button

---
+++ lib/feature/auth/presentation/login/ui/widgets/login_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/core/extensions/build_context_extensions.dart';
+import '../../../../../../core/resources/colors.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: context.width,
+          child: ElevatedButton(
+            style: Theme.of(context).elevatedButtonTheme.style,
+            onPressed: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Text(
+                "Login",
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium
+                    ?.copyWith(color: AppColors.white),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Don't have an account?",
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            TextButton(
+              onPressed: (){},
+              child: Text(
+                "Signup",
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium
+                    ?.copyWith(decoration: TextDecoration.underline, color: AppColors.primary),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
