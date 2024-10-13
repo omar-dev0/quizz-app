2676b0c00a3f8ec2fb0d620315b6bbeb47b94ccc
ali72-20 <as20201118@gmail.com>
2024-10-13 22:40:19 +0300

    add base urls file add end points file

--- lib/feature/auth/data/api/api_services.dart
+++ lib/feature/auth/data/api/api_services.dart
@@ -1,16 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:quizz_app/feature/auth/data/api/model/response/login_response.dart';
+import 'package:quizz_app/feature/auth/data/core/end_points.dart';
 import 'package:retrofit/retrofit.dart';
+import '../core/base_url.dart';
 import 'model/request/login_request.dart';
 
 part 'api_services.g.dart';
 @lazySingleton
-@RestApi(baseUrl: "https://exam.elevateegy.com//api/v1/auth/")
+@RestApi(baseUrl: BaseUrl.authBaseUrl)
 abstract class ApiServices{
   @factoryMethod
   factory ApiServices(Dio dio) = _ApiServices;
 
-  @POST('signin')
+  @POST(EndPoints.loginEndPoint)
   Future<LoginResponse> login(@Body() LoginRequest loginRequest);
 }
