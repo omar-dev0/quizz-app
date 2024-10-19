450dbcec4c7009d44ddf8cce8790d1e62db1b1d2
ali72-20 <as20201118@gmail.com>
2024-10-19 17:12:15 +0300

    add: di config

---
+++ lib/core/di/di.config.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+import '../../feature/exam/domain/repositories/home_repo.dart' as _i387;
+import '../../feature/exam/domain/use_cases/home_usecase.dart' as _i341;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    gh.factory<_i341.HomeUseCase>(
+        () => _i341.HomeUseCase(gh<_i387.HomeRepo>()));
+    return this;
+  }
+}
