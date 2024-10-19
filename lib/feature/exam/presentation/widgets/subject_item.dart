7e2ec1709c5bfe576f70f6dd686ef0073755de36
ali72-20 <as20201118@gmail.com>
2024-10-19 16:41:57 +0300

    add: subject item field

--- lib/feature/exam/presentation/widgets/subject_item.dart
+++ lib/feature/exam/presentation/widgets/subject_item.dart
@@ -7,28 +7,32 @@ class SubjectItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-      width: context.width,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.white),
-        boxShadow: [
-          BoxShadow(
-              color: Colors.black.withOpacity(0.15),
-              blurRadius: 4,
-              spreadRadius: 1)
-        ],
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          const Icon(Icons.language),
-          const SizedBox(
-            width: 8,
-          ),
-          Text("Language", style: Theme.of(context).textTheme.labelMedium)
-        ],
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        width: context.width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.white),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.15),
+                blurRadius: 4,
+                spreadRadius: 1)
+          ],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            const Icon(Icons.language),
+            const SizedBox(
+              width: 8,
+            ),
+            Text("Language", style: Theme.of(context).textTheme.labelMedium)
+          ],
+        ),
       ),
     );
   }
