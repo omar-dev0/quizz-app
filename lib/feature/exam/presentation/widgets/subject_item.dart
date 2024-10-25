68a0a446e88ffc018de9031352fab9616fa4925b
ali72-20 <as20201118@gmail.com>
2024-10-26 00:15:36 +0300

    update ui after fetch subject response

--- lib/feature/exam/presentation/widgets/subject_item.dart
+++ lib/feature/exam/presentation/widgets/subject_item.dart
@@ -1,18 +1,22 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quizz_app/core/extensions/build_context_extensions.dart';
+import 'package:quizz_app/feature/exam/presentation/manager/view_model.dart';
 
 class SubjectItem extends StatelessWidget {
   const SubjectItem({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final homeViewModel = context.read<HomeViewModel>();
+    int index = homeViewModel.getCurrentSubjectListIndex();
     return GestureDetector(
-      onTap: (){},
+      onTap: () {},
       child: Container(
         margin: const EdgeInsets.only(bottom: 16),
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
         width: context.width,
+        height: 80,
         decoration: BoxDecoration(
           color: Colors.white,
           border: Border.all(color: Colors.white),
@@ -26,11 +30,17 @@ class SubjectItem extends StatelessWidget {
         ),
         child: Row(
           children: [
-            const Icon(Icons.language),
+            SizedBox(
+              height: 48,
+              width: 48,
+              child: Image.network(
+                  homeViewModel.actionHandler.subjectsList[index].icon!),
+            ),
             const SizedBox(
               width: 8,
             ),
-            Text("Language", style: Theme.of(context).textTheme.labelMedium)
+            Text(homeViewModel.actionHandler.subjectsList[index].name!,
+                style: Theme.of(context).textTheme.labelMedium)
           ],
         ),
       ),
