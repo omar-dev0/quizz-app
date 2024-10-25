201cff487c6318c42e39b23ea90b2e3aabea0608
ali72-20 <as20201118@gmail.com>
2024-10-25 21:42:13 +0300

    edit: get subject action

--- lib/feature/exam/presentation/manager/action_handler.dart
+++ lib/feature/exam/presentation/manager/action_handler.dart
@@ -1,27 +1,37 @@
+import 'package:quizz_app/feature/exam/domain/entities/subject_item_entity.dart';
 import 'package:quizz_app/feature/exam/domain/use_cases/home_usecase.dart';
 import 'package:quizz_app/feature/exam/presentation/manager/home_screen_actions.dart';
 import 'package:quizz_app/feature/exam/presentation/manager/home_screen_states.dart';
 import 'package:quizz_app/feature/exam/presentation/manager/view_model.dart';
 
-class ActionHandler{
+class ActionHandler {
   late HomeViewModel _homeViewModel;
   final HomeUseCase _homeUseCase;
+  List<SubjectItemEntity> subjectsList = [];
+
   ActionHandler(this._homeUseCase, this._homeViewModel);
 
-
-  void handelActions(HomeActions action){
+  void handelActions(HomeActions action) {
     switch (action) {
       case GetSubjectsAction():
         _getSubjects();
         break;
       case NavigateToSubjectScreen():
-        // TODO: Handle this case.
+      // TODO: Handle this case.
     }
   }
 
-  void _getSubjects() async{
+  void _getSubjects() async {
     _homeViewModel.emitStat(LoadingState());
     final response = await _homeUseCase.invoke();
-
+    response.fold(
+      (fail) {
+        _homeViewModel.emitStat(FailState());
+      },
+      (subjects) {
+        subjectsList = subjects;
+        _homeViewModel.emitStat(SuccessState());
+      },
+    );
   }
 }
