9fdd4d47283ea55460143bee77ba2ff93a66879f
ali72-20 <as20201118@gmail.com>
2024-10-31 23:08:19 +0200

    fix loading indicator

--- lib/feature/exam/presentation/pages/exam_start_screen.dart
+++ lib/feature/exam/presentation/pages/exam_start_screen.dart
@@ -2,12 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ExamStartScreen extends StatelessWidget {
-  const ExamStartScreen({super.key});
+  final String examId;
+  const ExamStartScreen({super.key, required this.examId});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("data"),),
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Exam details"),
+        leading: IconButton(onPressed: (){
+          Navigator.pop(context);
+        }, icon: const Icon(Icons.arrow_back_ios_new) )
+      ),
+      body: const Center(child: Text("data"),),
     );
   }
 }
--- lib/feature/exam/presentation/widgets/exams_bloc.dart
+++ lib/feature/exam/presentation/widgets/exams_bloc.dart
@@ -4,10 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:quizz_app/core/extensions/build_context_extensions.dart';
+import 'package:quizz_app/feature/exam/presentation/manager/home_managers/home_screen_states.dart';
 import 'package:quizz_app/feature/exam/presentation/manager/subject_exam_manager/exams_screen_states.dart';
 import 'package:quizz_app/feature/exam/presentation/manager/subject_exam_manager/exams_view_model.dart';
 import 'package:quizz_app/feature/exam/presentation/pages/exam_start_screen.dart';
-import 'package:quizz_app/feature/exam/presentation/widgets/exam_item_container.dart';
 import 'package:quizz_app/feature/exam/presentation/widgets/exams_list_view.dart';
 
 import '../../../../core/resources/colors.dart';
@@ -19,32 +19,37 @@ class ExamsBloc extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<ExamsViewModel, ExamsScreenStates>(
         builder: (context, state) {
-          if(state is ExamsSuccessState){
-            return const ExamsListView();
-          }else if(state is ExamsFailState){
-            Fluttertoast.showToast(
-              msg: state.failMessage,
-              backgroundColor: AppColors.error,
-              textColor: AppColors.white,
-              toastLength: Toast.LENGTH_SHORT,
-            );
-          }else{
-            return SizedBox(
-              height: 200,
-              width: context.width,
-              child: const Center(
-                child: SpinKitFadingCircle(
-                  color: AppColors.primary,
-                ),
-              ),
-            );
-          }
-          return const ExamItemContainer();
-        }, listener: (context, state) {
-          if(state is NavigateToStartExamScreenState){
-              Navigator.of(context).push(MaterialPageRoute(builder: (_)=> const ExamStartScreen()));
-          }
-    }
-    );
+      if (state is ExamsSuccessState) {
+        return const ExamsListView();
+      } else if (state is ExamsFailState) {
+        Fluttertoast.showToast(
+          msg: state.failMessage,
+          backgroundColor: AppColors.error,
+          textColor: AppColors.white,
+          toastLength: Toast.LENGTH_SHORT,
+        );
+      } else if(state is LoadingState) {
+        return SizedBox(
+          height: 200,
+          width: context.width,
+          child: const Center(
+            child: SpinKitFadingCircle(
+              color: AppColors.primary,
+            ),
+          ),
+        );
+      }
+      return const ExamsListView();
+    }, listener: (context, state) {
+      if (state is NavigateToStartExamScreenState) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => ExamStartScreen(
+              examId: state.examId,
+            ),
+          ),
+        );
+      }
+    });
   }
 }
