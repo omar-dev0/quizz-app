6613203d186898e4ada08fb0f191da39bef41da1
omar-dev0 <11410120211067@stud.cu.edu.eg>
2024-11-07 23:06:20 +0200

    change hive box ids correct some merging things

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -8,99 +8,97 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i361;
-import 'package:get_it/get_it.dart' as _i174;
-import 'package:injectable/injectable.dart' as _i526;
-import 'package:pretty_dio_logger/pretty_dio_logger.dart' as _i528;
+import 'package:dio/dio.dart' as _i3;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+import 'package:pretty_dio_logger/pretty_dio_logger.dart' as _i6;
 
-import '../../feature/exam/data/apis/api_services.dart' as _i578;
-import '../../feature/exam/data/core/dio_provider.dart' as _i419;
+import '../../feature/exam/data/apis/api_services.dart' as _i8;
+import '../../feature/exam/data/core/dio_provider.dart' as _i23;
 import '../../feature/exam/data/data_sources/offline_data_source/offline_data_source.dart'
-    as _i82;
+    as _i4;
 import '../../feature/exam/data/data_sources/offline_data_source/offline_data_source_impl.dart'
-    as _i505;
+    as _i5;
 import '../../feature/exam/data/data_sources/online_data_source/online_data_source.dart'
-    as _i71;
+    as _i9;
 import '../../feature/exam/data/data_sources/online_data_source/online_data_source_impl.dart'
-    as _i685;
+    as _i10;
 import '../../feature/exam/data/repositories/exam_questions_repo_impl.dart'
-    as _i644;
-import '../../feature/exam/data/repositories/exam_repo_impl.dart' as _i733;
-import '../../feature/exam/data/repositories/home_repo_impl.dart' as _i815;
-import '../../feature/exam/domain/repositories/exam_repo.dart' as _i278;
+    as _i12;
+import '../../feature/exam/data/repositories/exam_repo_impl.dart' as _i14;
+import '../../feature/exam/data/repositories/home_repo_impl.dart' as _i16;
+import '../../feature/exam/domain/repositories/exam_repo.dart' as _i13;
 import '../../feature/exam/domain/repositories/exams_quesions_repo.dart'
-    as _i587;
-import '../../feature/exam/domain/repositories/home_repo.dart' as _i387;
-import '../../feature/exam/domain/use_cases/usecases.dart' as _i874;
+    as _i11;
+import '../../feature/exam/domain/repositories/home_repo.dart' as _i15;
+import '../../feature/exam/domain/use_cases/usecases.dart' as _i17;
 import '../../feature/exam/presentation/manager/exam_start_screen_manager/exam_start_view_model.dart'
-    as _i310;
+    as _i18;
 import '../../feature/exam/presentation/manager/home_managers/action_handler.dart'
-    as _i831;
+    as _i21;
 import '../../feature/exam/presentation/manager/home_managers/view_model.dart'
-    as _i399;
+    as _i20;
 import '../../feature/exam/presentation/manager/questions_screen_manager/questions_view_model.dart'
-    as _i226;
+    as _i7;
 import '../../feature/exam/presentation/manager/subject_exam_manager/exams_screen_action_handler.dart'
-    as _i339;
+    as _i22;
 import '../../feature/exam/presentation/manager/subject_exam_manager/exams_view_model.dart'
-    as _i820;
+    as _i19;
 
-extension GetItInjectableX on _i174.GetIt {
+extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
-  _i174.GetIt init({
+  _i1.GetIt init({
     String? environment,
-    _i526.EnvironmentFilter? environmentFilter,
+    _i2.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i526.GetItHelper(
+    final gh = _i2.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
     final networkModule = _$NetworkModule();
-    gh.factory<_i528.PrettyDioLogger>(
-        () => networkModule.providerInterceptor());
-    gh.factory<_i226.QuestionsScreenViewModel>(
-        () => _i226.QuestionsScreenViewModel());
-    gh.lazySingleton<_i361.Dio>(() => networkModule.provideDio());
-    gh.factory<_i82.OfflineDataSource>(() => _i505.OfflineDataSourceImpl());
-    gh.lazySingleton<_i578.ApiServices>(
-        () => _i578.ApiServices(gh<_i361.Dio>()));
-    gh.factory<_i71.OnlineDataSource>(
-        () => _i685.OnlineDataSourceImpl(gh<_i578.ApiServices>()));
-    gh.factory<_i387.HomeRepo>(() => _i815.HomeRepoImpl(
-          gh<_i71.OnlineDataSource>(),
-          gh<_i82.OfflineDataSource>(),
+    gh.lazySingleton<_i3.Dio>(() => networkModule.provideDio());
+    gh.factory<_i4.OfflineDataSource>(() => _i5.OfflineDataSourceImpl());
+    gh.factory<_i6.PrettyDioLogger>(() => networkModule.providerInterceptor());
+    gh.factory<_i7.QuestionsScreenViewModel>(
+        () => _i7.QuestionsScreenViewModel());
+    gh.lazySingleton<_i8.ApiServices>(() => _i8.ApiServices(gh<_i3.Dio>()));
+    gh.factory<_i9.OnlineDataSource>(
+        () => _i10.OnlineDataSourceImpl(gh<_i8.ApiServices>()));
+    gh.factory<_i11.ExamQuestionsRepo>(() => _i12.ExamQuestionsRepoImpl(
+          gh<_i9.OnlineDataSource>(),
+          gh<_i4.OfflineDataSource>(),
         ));
-    gh.factory<_i587.ExamQuestionsRepo>(() => _i644.ExamQuestionsRepoImpl(
-          gh<_i71.OnlineDataSource>(),
-          gh<_i82.OfflineDataSource>(),
+    gh.factory<_i13.ExamRepo>(() => _i14.ExamRepoImpl(
+          gh<_i9.OnlineDataSource>(),
+          gh<_i4.OfflineDataSource>(),
         ));
-    gh.factory<_i278.ExamRepo>(() => _i733.ExamRepoImpl(
-          gh<_i71.OnlineDataSource>(),
-          gh<_i82.OfflineDataSource>(),
+    gh.factory<_i15.HomeRepo>(() => _i16.HomeRepoImpl(
+          gh<_i9.OnlineDataSource>(),
+          gh<_i4.OfflineDataSource>(),
         ));
-    gh.factory<_i874.UseCases>(() => _i874.UseCases(
-          gh<_i387.HomeRepo>(),
-          gh<_i278.ExamRepo>(),
-          gh<_i587.ExamQuestionsRepo>(),
+    gh.factory<_i17.UseCases>(() => _i17.UseCases(
+          gh<_i15.HomeRepo>(),
+          gh<_i13.ExamRepo>(),
+          gh<_i11.ExamQuestionsRepo>(),
         ));
-    gh.factory<_i310.ExamStartScreenViewModel>(
-        () => _i310.ExamStartScreenViewModel(gh<_i874.UseCases>()));
-    gh.factory<_i399.HomeViewModel>(
-        () => _i399.HomeViewModel(gh<_i874.UseCases>()));
-    gh.factory<_i820.ExamsViewModel>(
-        () => _i820.ExamsViewModel(gh<_i874.UseCases>()));
-    gh.singleton<_i339.ExamsScreenActionHandler>(
-        () => _i339.ExamsScreenActionHandler(
-              gh<_i820.ExamsViewModel>(),
-              gh<_i874.UseCases>(),
+    gh.factory<_i18.ExamStartScreenViewModel>(
+        () => _i18.ExamStartScreenViewModel(gh<_i17.UseCases>()));
+    gh.factory<_i19.ExamsViewModel>(
+        () => _i19.ExamsViewModel(gh<_i17.UseCases>()));
+    gh.factory<_i20.HomeViewModel>(
+        () => _i20.HomeViewModel(gh<_i17.UseCases>()));
+    gh.singleton<_i21.ActionHandler>(() => _i21.ActionHandler(
+          gh<_i17.UseCases>(),
+          gh<_i20.HomeViewModel>(),
+        ));
+    gh.singleton<_i22.ExamsScreenActionHandler>(
+        () => _i22.ExamsScreenActionHandler(
+              gh<_i19.ExamsViewModel>(),
+              gh<_i17.UseCases>(),
             ));
-    gh.singleton<_i831.ActionHandler>(() => _i831.ActionHandler(
-          gh<_i874.UseCases>(),
-          gh<_i399.HomeViewModel>(),
-        ));
     return this;
   }
 }
 
-class _$NetworkModule extends _i419.NetworkModule {}
+class _$NetworkModule extends _i23.NetworkModule {}
